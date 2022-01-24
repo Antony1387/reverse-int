c8e3025bd9fcefe69c1f2fd6e759fac7554dc270
Antony1387 <79935548+Antony1387@users.noreply.github.com>
2022-01-24 23:44:37 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function revString(str) {
-    var splitString = str.split("");
-    var revArray = splitString.reverse();
-    var joinArray = revArray.join("");
-    return joinArray;
-}
-console.log(revString("1234"));
-
-let num = Number(revString);
